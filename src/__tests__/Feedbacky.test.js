9e36a2767b3e1e6aff96f2f7b5e8389f479e4de3
Ilgaz Zararsız <ilgazzararsiz@gmail.com>
2022-03-24 01:31:22 +0300

    unnecessary import removed.

--- src/__tests__/Feedbacky.test.js
+++ src/__tests__/Feedbacky.test.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import {render} from "@testing-library/react";
 import {Feedbacky} from '../components/Feedbacky'
-import {FeedbackyModal} from '../components/FeedbackyModal'
 
 test('Should matches Feedbacky with snapshot', () => {
     const {container} = render(<Feedbacky/>)
