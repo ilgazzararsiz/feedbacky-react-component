4b89e258f8373ed757fa69d907c3068e1bd03325
Ilgaz Zararsız <ilgazzararsiz@gmail.com>
2022-03-24 17:03:35 +0300

    request changed axios to fetch.

--- src/components/FeedbackyModal/FeedbackyModal.js
+++ src/components/FeedbackyModal/FeedbackyModal.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react'
-import axios from "axios";
 import closeButton from '../../assets/images/close.png'
 import {feedback} from '../../services/service'
 
--- src/services/service.js
+++ src/services/service.js
@@ -1,13 +1,19 @@
-const axios = require("axios")
-
 export const feedback = {
     update: async (value) => {
         try {
-            const res = await axios.post(value.baseUrl, {
-                content: value.feedbackContent,
-                applicationId: value.applicationId
-            })
-            return Promise.resolve(res.data)
+            console.log("value", value)
+            const res = await fetch(value.baseUrl, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    content: value.feedbackContent,
+                    applicationId: value.applicationId
+                })
+            });
+            return Promise.resolve(res)
         } catch (err) {
             return Promise.reject(err)
         }
